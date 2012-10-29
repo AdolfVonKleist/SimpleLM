c306b9d47d88fdb1b8304467310c925c9fabc0e3
AdolfVonKleist <josef.robert.novak@gmail.com>
2012-10-29 18:12:31 +0900

    Added some additional functionality and comments to evaluate-lm.py.

--- evaluate-lm.py
+++ evaluate-lm.py
@@ -108,10 +108,12 @@ def load_arpa( arpa_file ):
 def compute_sentence_prob( arpalm, sentence ):
     """
       Compute the probability of the input sentence.
-      Should produce the same output as the SRILM tool,
+      Should produce the same output as the SRILM ngram tool,
 
         $ ngram -lm test.arpa -ppl sent.txt
-      where sent.txt contains the input sentence.
+      or the NGramLibrary ngramapply tool, to within 
+      a delta of about 1e-7.  Longer sentences may diverge
+      further due to rounding.
     """
 
     total = 0.0
@@ -190,18 +192,86 @@ def retrieve_ngram_prob( arpalm, sentence ):
     # of the input sentence
     return total
 
+
+def history_sum( arpalm, ngram_hist ):
+    """
+      Compute the sum for a given history.
+      This will perform a sum over all n+1 grams starting with ngram_hist,
+      as well as any backoff probs for items in the vocabulary that do NOT
+      occur as an extension of ngram_hist.  
+
+      The result should always be 1.0~ unless the NGram ends in </s>.
+    """
+
+    from math import pow
+
+    #The vocabulary is just the set of unigrams
+    vocab = set(arpalm.children.keys())
+    
+    total = 0.0
+
+    #Now compute the sum over all extensions of the history
+    for word in vocab:
+        ngram = [ w for w in ngram_hist ]
+        ngram.append(word)
+        total += pow(10,retrieve_ngram_prob( arpalm, ngram ))
+    
+    return total
+
+def lm_is_normalized( arpalm_file, delta=1e-6 ):
+    """
+      Determine whether the model is fully normalized.
+      
+      First, for the zero-th order, compute the unigram sum.
+      Next, for each ngram order o<max_order, compute the history_sum.
+
+      If the absolute value,  abs(1-hist_sum)<delta, then count 
+      the history as normalized.  Otherwise through a ValueError and quit.
+    """
+    from math import pow
+
+    order = 0
+    arpalm = load_arpa( arpalm_file )
+    
+    total = 0.0
+    #First the unigram model
+    for word in arpalm.children.keys():
+        total += pow(10., float(arpalm.children[word].prob))
+    if abs(1.-total)>delta:
+        print total, delta, 1.-total
+        raise ValueError, "The unigram model is not fully normalized!"
+
+    total = 0.0
+    for line in open(arpalm_file,"r"):
+        line = line.strip()
+        if order>0 and not line.startswith("\\") and not line=="":
+            parts = line.split("\t")
+            ngram = parts[1].split(" ")
+            total += pow(10., history_sum(arpalm,ngram))
+        if re.match(r"^\\\d+",line):
+            if order>0 and 1-total > delta:
+                raise ValueError, "The %d-order model is not fully normalized!"%(order)
+            order = int(re.sub(r"^\\(\d+).*$",r"\1",line))
+            #Return True if we've reached the max_order
+            if order==arpalm.max_order:
+                return True
+            #reset the total since we have a new order
+            total = 0.0
+
 if __name__=="__main__":
     import sys, argparse
 
     example = """%s --arpalm lm.arpa --sent "some sentence to evaluate" """ % sys.argv[0]
     parser = argparse.ArgumentParser(description=example)
-    parser.add_argument('--arpalm',  "-m",   help="The ARPA format language model to be used.", required=True )
-    parser.add_argument('--sent',    "-s",   help="The input sentence/sequence to be evaluated.", required=True )
-    parser.add_argument('--sb',      "-b",   help="The sentence begin token. (<s>)", default="<s>", required=False )
-    parser.add_argument('--se',      "-e",   help="The sentence begin token. (</s>)", default="</s>", required=False )
-    parser.add_argument('--nosbse',  "-n",   help="Don't add sentence-begin/sentence-end tokens to the input.", default=False, action="store_true" )
+    parser.add_argument('--arpalm',    "-m",   help="The ARPA format language model to be used.", required=True )
+    parser.add_argument('--sent',      "-s",   help="The input sentence/sequence to be evaluated.", required=False, default="" )
+    parser.add_argument('--sb',        "-b",   help="The sentence begin token. (<s>)", default="<s>", required=False )
+    parser.add_argument('--se',        "-e",   help="The sentence begin token. (</s>)", default="</s>", required=False )
+    parser.add_argument('--nosbse',    "-n",   help="Don't add sentence-begin/sentence-end tokens to the input.", default=False, action="store_true" )
+    parser.add_argument('--hist_sum',  "-i",   help="Compute the sum for a given history.  History must be in the model.", default=False, action="store_true" ) 
     parser.add_argument('--get_ngram', "-g", help="Retrieve an individual NGram probability. Length of the NGrm  must be <= the max order of the input LM.", 
                         default=False, action="store_true" )
+    parser.add_argument('--delta',     "-d", help="Delta for determining normalization success/failure.", type=float, default=1e-6, required=False )
     parser.add_argument('--verbose', "-v", help="Verbose mode.", action="store_true", default=False )
     args = parser.parse_args()
 
@@ -209,29 +279,34 @@ if __name__=="__main__":
         for attr, value in args.__dict__.iteritems():
             print attr, "=", value
 
-    arpalm = load_arpa( args.arpalm )
-
-    tokens = args.sent.split(" ")
-
-    for token in tokens:
-        if token not in arpalm.children:
-            raise ValueError, "Unigram token: %s not found in LM vocabulary!" % (token)
-
-    if args.get_ngram:
-        if len(tokens)>arpalm.max_order:
-            print "Sequence:", tokens, "is longer than the max order of the input model!"
-            sys.exit(1)
-        print "NGram:", tokens
-        print "NGram prob:", retrieve_ngram_prob( arpalm, tokens )
+    if args.sent=="":
+        print lm_is_normalized( args.arpalm, delta=args.delta )
     else:
-        if args.nosbse:
-            print "Not adding sentence-begin/sentence-end tokens."
+        arpalm = load_arpa( args.arpalm )
+
+        tokens = args.sent.split(" ")
+
+        for token in tokens:
+            if token not in arpalm.children:
+                raise ValueError, "Unigram token: %s not found in LM vocabulary!" % (token)
+
+        if args.get_ngram:
+            if len(tokens)>arpalm.max_order:
+                print "Sequence:", tokens, "is longer than the max order of the input model!"
+                sys.exit(1)
+            print "NGram:", tokens
+            print "NGram prob:", retrieve_ngram_prob( arpalm, tokens )
+        elif args.hist_sum:
+            print history_sum( arpalm, args.sent.split(" ") )
         else:
-            if not tokens[0]==args.sb:
-                tokens.insert(0,args.sb)
-            if not tokens[-1]==args.se:
-                tokens.append(args.se)
-        print "Evaluating sequence:", " ".join(tokens)
-        print "Log_10 prob:", compute_sentence_prob( arpalm, tokens )
+            if args.nosbse:
+                print "Not adding sentence-begin/sentence-end tokens."
+            else:
+                if not tokens[0]==args.sb:
+                    tokens.insert(0,args.sb)
+                if not tokens[-1]==args.se:
+                    tokens.append(args.se)
+            print "Evaluating sequence:", " ".join(tokens)
+            print "Log_10 prob:", compute_sentence_prob( arpalm, tokens )
 
     
